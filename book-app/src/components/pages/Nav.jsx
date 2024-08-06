aa542dd3eba900db6e63ddf15f4a81c190fd467f
Gabriella <gabriella.mabento@gmail.com>
2024-08-06 17:35:35 -0300

    feat: adição de dinamismo na Nav de acordo com o userId

--- book-app/src/components/pages/Nav.jsx
+++ book-app/src/components/pages/Nav.jsx
@@ -1,8 +1,15 @@
 
 import { Link } from 'react-router-dom'
 import { HStack, Button, Box } from '@chakra-ui/react';
+import { useContext } from 'react';
+import UserIdContext from '../contexts/UserIdContext';
 
 const Nav = () => {
+  const { userId, setUserId } = useContext(UserIdContext);
+
+  const logout = () => {
+    setUserId(null)
+  }
 
   return (
     <nav>
@@ -10,9 +17,18 @@ const Nav = () => {
       <Box p='4'>
         <HStack spacing={{ base: '1em', sm: '2em', md: '4em', lg: '8em' }}>
           <Link to={'/'}><Button variant='link' colorScheme='brand'>Home</Button></Link>
-          <Link to={'/bookshelf'}><Button variant='link' colorScheme='brand'>Bookshelf</Button></Link>
-          <Link to={'/login'}><Button variant='link' colorScheme='brand'>Log in</Button></Link>
-          <Link to={'/signup'}><Button variant='link' colorScheme='brand'>Sign up</Button></Link>
+          
+         {!userId ? (
+          <>
+             <Link to={'/login'}><Button variant='link' colorScheme='brand'>Log in</Button></Link>
+             <Link to={'/signup'}><Button variant='link' colorScheme='brand'>Sign up</Button></Link>
+          </>
+         ) : (
+            <>
+              <Link to={'/bookshelf'}><Button variant='link' colorScheme='brand'>Bookshelf</Button></Link>
+              <Button variant='link' colorScheme='brand' onClick={logout}>Sair</Button>
+            </>
+         )}
         </HStack>
       </Box>
         
