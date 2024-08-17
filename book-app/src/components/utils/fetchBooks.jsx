8011d925702f6149ad56cb5246f114a2e372d512
Gabriella <gabriella.mabento@gmail.com>
2024-08-17 16:59:12 -0300

    fix: resolver issue com imagens

--- book-app/src/components/utils/fetchBooks.jsx
+++ book-app/src/components/utils/fetchBooks.jsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 import { db } from '../../firebaseConfig';
 import { collection, addDoc, getDocs, query, where } from 'firebase/firestore';
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { storage } from '../../firebaseConfig'
 
 const searchBookByTitle = async (title) => {
     try {
@@ -28,6 +30,15 @@ const searchBookByTitle = async (title) => {
     }
 };
 
+const uploadImage = async (imageUrl) => {
+    try {
+        const response = await axios.post('http://localhost:5000/upload', { imageUrl });
+        return response.data.imageUrl;
+    } catch (error) {
+        console.error('Erro ao fazer upload da imagem:', error);
+        return null;
+    }
+};
 
 export const fetchBookDetailsById = async (volumeId) => {
     try {
@@ -45,12 +56,17 @@ export const fetchBookDetailsById = async (volumeId) => {
 
  
         if (existingBooksSnapshot.empty) {
+
+            let thumbnailUrl = book.imageLinks?.extraLarge || book.imageLinks?.large || book.imageLinks?.medium || book.imageLinks?.thumbnail;
+            const firebaseImageUrl = thumbnailUrl ? await uploadImage(thumbnailUrl) : 'Imagem não disponível';
+            console.log(firebaseImageUrl)
+            
             // Dados do livro a serem adicionados ao Firestore
             const bookData = {
                 title: book.title,
                 authors: book.authors ? book.authors.join(', ') : 'Autor desconhecido',
                 description: book.description || 'Sem descrição',
-                thumbnail: book.imageLinks?.large || book.imageLinks?.medium || book.imageLinks?.thumbnail || 'Imagem não disponível',
+                thumbnail: firebaseImageUrl,
                 publishedDate: book.publishedDate || 'Data desconhecida',
                 pageCount: book.pageCount || 0,
                 categories: book.categories || [],
